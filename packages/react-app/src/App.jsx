88541c95748c97928365892832a2a02606be4bf9
fainashalts <faina@trufflesuite.com>
2022-01-27 20:28:35 -0800

    Rename hardhat_contracts.json --> deployed_contracts.json

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -27,7 +27,7 @@ import {
 import { NETWORKS, ALCHEMY_KEY } from "./constants";
 import externalContracts from "./contracts/external_contracts";
 // contracts
-import deployedContracts from "./contracts/hardhat_contracts.json";
+import deployedContracts from "./contracts/deployed_contracts.json";
 import { Transactor, Web3ModalSetup } from "./helpers";
 import { Home, ExampleUI, Hints, Subgraph } from "./views";
 import { useStaticJsonRPC } from "./hooks";
--- packages/react-app/src/helpers/loadAppContracts.js
+++ packages/react-app/src/helpers/loadAppContracts.js
@@ -1,4 +1,4 @@
-const contractListPromise = import("../contracts/hardhat_contracts.json");
+const contractListPromise = import("../contracts/deployed_contracts.json");
 // @ts-ignore
 const externalContractsPromise = import("../contracts/external_contracts");
 
